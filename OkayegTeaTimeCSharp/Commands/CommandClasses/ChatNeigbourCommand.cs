6d3743ef4ed67094b795371ec8c1a65d1d4a434e
Jann Amhausend <58480669+jann-amh@users.noreply.github.com>
2021-06-13 17:14:25 +0200

    added chatneighbour function

---
+++ OkayegTeaTimeCSharp/Commands/CommandClasses/ChatNeigbourCommand.cs
@@ -0,0 +1,22 @@
+﻿using OkayegTeaTimeCSharp.HttpRequests;
+using OkayegTeaTimeCSharp.Twitch;
+using OkayegTeaTimeCSharp.Twitch.Bot;
+using OkayegTeaTimeCSharp.Utils;
+using System.Collections.Generic;
+using System.Linq;
+using TwitchLib.Client.Models;
+
+namespace OkayegTeaTimeCSharp.Commands.CommandClasses
+{
+    public static class ChatNeighbourCommand
+    {
+        public static void Handle(TwitchBot twitchBot, ChatMessage chatMessage, string alias)
+        {
+            List<string> chatters = HttpRequest.GetChatters(chatMessage.Channel).OrderByDescending(chatters => chatters).ToList();
+            string chatter1 = chatters[chatters.IndexOf(chatMessage.Username) - 1];
+            string chatter2 = chatters[chatters.IndexOf(chatMessage.Username) + 1];
+            twitchBot.Send(chatMessage.Channel, "Your chatneighbours are " + chatter1 + " and " + chatter2);
+        }
+    }
+
+}
